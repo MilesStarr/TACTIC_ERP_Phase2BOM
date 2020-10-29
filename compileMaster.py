04057e06fe29b65b56c0c24c6c33196c77078117
Elton Clark <eclark@labtesting.com>
2020-10-29 09:59:15 -0400

    Added tanks to master XML, included code for generating/appending to master XML

---
+++ compileMaster.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 22 13:24:55 2020
+
+@author: eclark
+"""
+
+from lxml import etree
+import re
+
+def check(testPN, partSet):
+    testPN = ".*{}.*".format(testPN)
+    hits = []
+    for part in partSet:
+        if re.match(testPN, part):
+            hits.append(part)
+    return hits
+
+#add new files here to validate that parts are not already defined in the master
+files = ["Tanks.xml"]
+
+masterTree = etree.parse(".TacTicMasterBOM.xml")
+masterRoot = masterTree.getroot()
+
+for file in files:
+    thisTree = etree.parse(file)
+    theseParts = thisTree.getroot().findall(".//Part")
+    for thisPart in theseParts:
+        if masterRoot.find(".//Part[@PartID='{0}']".format(thisPart.attrib['PartID'])) is None:
+            masterRoot.append(thisPart)
+        else:
+            print("Crashed on part {} in {}".format(thisPart.attrib['PartID'], file))
+
+allParts = masterRoot.findall(".//Part")
+partDefs = set()
+for part in allParts:
+    partDefs.add(part.attrib['PartID'])
+
+#masterTree.write(".TacTicMasterBOM.xml") #uncomment to overwrite master BOM xml file
